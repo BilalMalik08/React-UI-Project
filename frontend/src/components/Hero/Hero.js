5574f7da93f9e1f51d1c9983decb67bb76c32c65
BilalMalik08 <muhammad.billal.cs@gmail.com>
2024-08-01 17:16:08 +0500

    Updated 5:15 PM , 1 August, 2024

--- frontend/src/components/Hero/Hero.js
+++ frontend/src/components/Hero/Hero.js
@@ -1,7 +1,16 @@
+import React from "react";
 import "./hero.css";
 
 function Hero() {
-  return <div className="Hero-Container"></div>;
+  return (
+    <div className="Hero-Container container-fluid">
+      <div className="Hero-Sub-Container">
+        <h1 className="Hero-heading Hero-heading-1">INVESTMENTS</h1>
+        <h6 className="Hero-heading Hero-heading-6">DUBAI</h6>
+        <button className="Hero-btn">Learn More</button>
+      </div>
+    </div>
+  );
 }
 
 export default Hero;
